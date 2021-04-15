0a9444ea0ae0ca3cb065b749b0bc9c2134a53dfc
thtngan <thtngan>
2021-04-15 15:27:21 +0700

    Add movable object: Elephant

---
+++ CElephant.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Header.h"
+#include "CObject.h"
+#include "CPos.h"
+
+class CElephant : public CObject
+{
+private:
+	char** _elephant;
+
+public:
+	CElephant();
+	~CElephant();
+
+	//return sth
+	char** kind();
+	int getW();
+	int getH();
+
+};
+
