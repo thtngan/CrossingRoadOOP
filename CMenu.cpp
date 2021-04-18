6449e2b38de0d61b26fd88f9ee1f541dbd5b9930
thtngan <thtngan>
2021-04-19 01:04:54 +0700

    Can choose option in the menu

--- CMenu.cpp
+++ CMenu.cpp
@@ -1,3 +1,6 @@
+#define _CRT_NONSTDC_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "CMenu.h"
 
 void CMenu::menu() {
@@ -5,46 +8,77 @@ void CMenu::menu() {
 
 	const string choice[5] = { "New Game", "Load Game", "Rank", "Settings", "Exit" };
 
-	int color = rand() % 16 + 1;
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-	gotoXY(10, 3); cout << " ______  ______   ______   ______   ______  _____  ______   ______     ______   ______   ______   _____   ";
-	gotoXY(10, 4); cout << "| |     | |  | \\ / |  | \\ / |      / |       | |  | |  \\ \\ | | ____   | |  | \\ / |  | \\ | |  | | | | \\ \\  ";
-	gotoXY(10, 5); cout << "| |     | |__| | | |  | | '------. '------.  | |  | |  | | | |  | |   | |__| | | |  | | | |__| | | |  | | ";
-	gotoXY(10, 6); cout << "|_|____ |_|  \\_\\ \\_|__|_/  ____|_/  ____|_/ _|_|_ |_|  |_| |_|__|_|   |_|  \\_\\ \\_|__|_/ |_|  |_| |_|_/_/  ";
-	gotoXY(10, 7); cout << "                                                                                                          ";
-
 	bool input = true;
 	int x = 35, y = 10, pos = 0;
-	if (input) {
-		//Print list of menu
-		for (int i = 0; i < 5; i++) {
-			gotoXY(x, y + i);
-			if (i == pos)
-			{
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+	
+	ShowConsoleCursor(false);
+
+	while (true) {
+		input = true;
+		while (true) {
+			if (input) {
+				//Print list of menu
+				for (int i = 0; i < 5; i++) {
+					gotoXY(x, y + i);
+					if (i == pos)
+					{
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+					}
+					else
+					{
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+					}
+					cout << choice[i] << endl;
+				}
+
+				int color = rand() % 16 + 1;
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+				gotoXY(10, 3); cout << " ______  ______   ______   ______   ______  _____  ______   ______     ______   ______   ______   _____   ";
+				gotoXY(10, 4); cout << "| |     | |  | \\ / |  | \\ / |      / |       | |  | |  \\ \\ | | ____   | |  | \\ / |  | \\ | |  | | | | \\ \\  ";
+				gotoXY(10, 5); cout << "| |     | |__| | | |  | | '------. '------.  | |  | |  | | | |  | |   | |__| | | |  | | | |__| | | |  | | ";
+				gotoXY(10, 6); cout << "|_|____ |_|  \\_\\ \\_|__|_/  ____|_/  ____|_/ _|_|_ |_|  |_| |_|__|_|   |_|  \\_\\ \\_|__|_/ |_|  |_| |_|_/_/  ";
+				gotoXY(10, 7); cout << "                                                                                                          ";
+
+				input = false;
+
 			}
-			else
-			{
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+			input = false;
+			switch (getch()) {
+				input = true;
+			case 72:
+				input = true;
+				pos--;
+				pos = (pos + 5) % 5;
+				break;
+			case 80:
+				input = true;
+				pos++;
+				pos %= 5;
+				break;
+			case 13:
+				switch (pos) {
+				case 0: //New game
+					while (1) {
+
+					}
+					break;
+				case 1: //Loadgame
+					break;
+				case 2: //Rank
+					//Rank
+					input = true;
+					break;
+				case 3: //Settings
+					//Settings
+					input = true;
+					break;
+				case 4: //Exit
+					system("cls");
+					return;
+				}
+				break;
 			}
-			cout << choice[i] << endl;
 		}
-	}
-	input = false;
 
-	switch (inputB()) {
-		input = true;
-		
-	case 'w' : case 'W':
-		input = true;
-		pos--;
-		pos = (pos + 5) % 5;
-		break;
-
-	case 's' : case 'S':
-		input = true;
-		pos++;
-		pos = (pos + 5) % 5;
-		break;
 	}
 }
--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,8 @@
 #include "CMap.h"
 #include "CMenu.h"
 
+
+
 using namespace std;
 
 int main() {
