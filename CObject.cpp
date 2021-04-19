f70ce1ad684dc79444e0165c12d9e087a14a69c9
MyrtilleKim <lthkim01@gmail.com>
2021-04-20 00:08:24 +0700

    Check out map & update position

--- CObject.cpp
+++ CObject.cpp
@@ -1,2 +1,23 @@
 #include "CObject.h"
 
+CObject::CObject() : _outMap(false){}
+CObject::CObject(const CPos& start) : _pos(start), _outMap(false){}
+int CObject::getX(){
+	return _pos.getX();
+}
+int CObject::getY(){
+	return _pos.getY();
+}
+CPos CObject::getPos(){
+	return _pos;
+}
+bool CObject::isOutMap(){
+	return _outMap;
+}
+void CObject::setOutMap(){
+	_outMap = true;
+}
+void CObject::updatePos(const int x, const int y){
+	_pos.setPos(_pos.getX() + x, _pos.getY() + y);
+}
+
--- CObject.h
+++ CObject.h
@@ -6,10 +6,20 @@ class CObject
 {
 private:
 	CPos _pos;
+	bool _outMap;
 	
 public:
-	CObject() = default;
+	int getX();
+	int getY();
+	CObject();
+	CObject(const CPos&);
 	~CObject() = default;
-
+	CPos getPos();
+	bool isOutMap();
+	virtual int getH() = 0;
+	virtual int getW() = 0;
+	void updatePos(const int x, const int y);
+	void setOutMap();
+	virtual char** kind() = 0;
 };
 
